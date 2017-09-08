266e0d5adc650bb01e235b676005be49a7826033
joql <981037735@qq.com>
2017-09-08 17:32:34 +0800

    test

---
+++ data/common.inc.php
@@ -0,0 +1,12 @@
+<?php
+//数据库连接信息
+$cfg_dbtype = 'mysql';
+$cfg_dbhost = 'bdm245672235.my3w.com';
+$cfg_dbname = 'bdm245672235_db';
+$cfg_dbuser = 'bdm245672235';
+$cfg_dbpwd = 'dbjoql001';
+$cfg_dbprefix = 'tp_dede_';
+$cfg_db_language = 'utf8';
+
+
+?>
